970b1881be4415988c494e63dd063647f00ff308
Reales <rclarence123@gmail.com>
2023-03-09 23:56:48 -0500

    Tipo de datos

---
+++ tipoDeDatos.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Hello, World!") //String
+	fmt.Println(24)              //int
+	fmt.Println(2.5)             //float 64 o float 32
+	fmt.Println(true)            // booleano
+}
